b1ec3b19080764d33df7b4f04a8d1031740bf9ed
costin <cosraducostache@gmail.com>
2018-10-07 21:39:50 +0300

    working my-center container yeeey

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Default Page</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="main.css?v=<? echo time() ?>" rel="stylesheet">
+
+</head>
+<body>
+<br>
+<br>
+<br>
+<div class="my-center-container">
+    <h3 class="cici">Web Project</h3>
+
+
+</div>
+
+</body>
+</html>
