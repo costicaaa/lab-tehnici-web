bab5a6d6135c8a8c92d5c2f4bd5570cc08e5fa3f
costin <you@example.com>
2018-10-08 16:59:24 +0300

    added article to make sure i have enough semantic elements

--- public/index.php
+++ public/index.php
@@ -11,42 +11,45 @@
 
 </head>
 <body>
-<br>
-<br>
-<br>
-<div class="my-center-container-title">
-    <h1 >Web Project</h1>
-</div>
-<hr>
+    <article>
+        <br>
+        <br>
+        <br>
+        <div class="my-center-container-title">
+            <h1>Web Project</h1>
+        </div>
+        <hr>
 
-<div class="my-wrapper-container">
-    <p>
-        This is another
-        <a href="/todo.php">ToDo</a> app example.
-    </p>
-    <br>
-    <p>
-        Unlike the usual demos, in this example we are not going to use something new and awesome, we are going to
-        re-invent the wheel, as per <span class="my-bold"><a href="https://github.com/WebToLearn/laborator-tehnici-web">requirements</a></span>.
-    </p>
+        <div class="my-wrapper-container">
+            <p>
+                This is another
+                <a href="/todo.php">ToDo</a> app example.
+            </p>
+            <br>
+            <p>
+                Unlike the usual demos, in this example we are not going to use something new and awesome, we are going
+                to
+                re-invent the wheel, as per <span class="my-bold"><a
+                            href="https://github.com/WebToLearn/laborator-tehnici-web">requirements</a></span>.
+            </p>
 
-    <br>
-    <br>
-    <p>
-        In this project, only plain css and vanilla js will be used.
-    </p>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+            <br>
+            <br>
+            <p>
+                In this project, only plain css and vanilla js will be used.
+            </p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
 
-    <div class="my-right-float">
-        <p> Costin Costache</p>
-        <p> Group 353</p>
-    </div>
-</div>
-
+            <div class="my-right-float">
+                <p> Costin Costache</p>
+                <p> Group 353</p>
+            </div>
+        </div>
+    </article>
 </body>
 </html>
